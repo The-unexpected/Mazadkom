b44f5904f5678d19365d8ec5153a38162d95e6da
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-08-03 22:20:35 +0300

    commit

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const express = require('express');
 const router = express.Router();
 const { productModel } = require('../models/product.schema');
@@ -10,15 +11,13 @@ const users = require('../src/auth/models/user.model');
 router.get('/', getProduct);
 router.get('/:id', getProduct);
 router.post('/:id', createProduct);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+// router.put('/:id', updateProduct);
+// router.delete('/:id', deleteProduct);
 
 
 async function getProduct(req, res, next) {
   let array = [];
   let userId = req.params.id;
-  // console.log(req);
-  // console.log(res);
   users.find({ _id: userId }, (error, data) => {
     data[0].bids.map((bid) => {
       array.push(bid);
@@ -28,18 +27,9 @@ async function getProduct(req, res, next) {
 }
 
 
-// try {
-//   const data = req.body;
-//   const newUser = await user.create(data);
-//   res.json(newUser);
-// } catch (e) {
-//   next(e);
-// }
 
 async function createProduct(req, res, next) {
-  // console.log('req', req, 'res', res);
   try {
-    let array = [];
     let userId = req.params.id;
     const data1 = req.body;
     const newProduct = await product.create(data1);
@@ -48,33 +38,32 @@ async function createProduct(req, res, next) {
       data[0].save();
       res.json(data[0].bids);
     });
-
   }
   catch (e) {
     console.log(e.message);
   }
 }
 
-async function updateProduct(req, res, next) {
-  try {
-    const id = req.params.id;
-    const data = req.body;
-    const newProduct = await product.update(id, data);
-    res.json(newProduct);
-  } catch (e) {
-    next(e);
-  }
-}
+// async function updateProduct(req, res, next) {
+//   try {
+//     const id = req.params.id;
+//     const data = req.body;
+//     const newProduct = await product.update(id, data);
+//     res.json(newProduct);
+//   } catch (e) {
+//     next(e);
+//   }
+// }
 
-async function deleteProduct(req, res, next) {
-  try {
-    const id = req.params.id;
-    const deletedProduct = await product.delete(id);
-    res.json(deletedProduct);
-  } catch (e) {
-    next(e);
-  }
-}
+// async function deleteProduct(req, res, next) {
+//   try {
+//     const id = req.params.id;
+//     const deletedProduct = await product.delete(id);
+//     res.json(deletedProduct);
+//   } catch (e) {
+//     next(e);
+//   }
+// }
 
 
 module.exports = router;
